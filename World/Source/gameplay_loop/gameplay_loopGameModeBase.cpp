8384cba10a4ec65eacc16343d5e53f785cc67e5c
Cameron Ayling Abertay <60215201+CameronAyling-abertay@users.noreply.github.com>
2022-02-23 01:35:54 +0000

    Action points implemented
    Press E to inquire into a sheep's activities. You can perform this action twice. Only decrements the action points at the end of the conversation.

--- World/Source/gameplay_loop/gameplay_loopGameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "gameplay_loopGameModeBase.h"
-
---
+++ gameplay_loop/Intermediate/Build/Win64/UE4Editor/Inc/gameplay_loop/gameplay_loopGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef GAMEPLAY_LOOP_gameplay_loopGameModeBase_generated_h
+#error "gameplay_loopGameModeBase.generated.h already included, missing '#pragma once' in gameplay_loopGameModeBase.h"
+#endif
+#define GAMEPLAY_LOOP_gameplay_loopGameModeBase_generated_h
+
+#define gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_SPARSE_DATA
+#define gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_RPC_WRAPPERS
+#define gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAgameplay_loopGameModeBase(); \
+	friend struct Z_Construct_UClass_Agameplay_loopGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(Agameplay_loopGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/gameplay_loop"), NO_API) \
+	DECLARE_SERIALIZER(Agameplay_loopGameModeBase)
+
+
+#define gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAgameplay_loopGameModeBase(); \
+	friend struct Z_Construct_UClass_Agameplay_loopGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(Agameplay_loopGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/gameplay_loop"), NO_API) \
+	DECLARE_SERIALIZER(Agameplay_loopGameModeBase)
+
+
+#define gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API Agameplay_loopGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(Agameplay_loopGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, Agameplay_loopGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(Agameplay_loopGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API Agameplay_loopGameModeBase(Agameplay_loopGameModeBase&&); \
+	NO_API Agameplay_loopGameModeBase(const Agameplay_loopGameModeBase&); \
+public:
+
+
+#define gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API Agameplay_loopGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API Agameplay_loopGameModeBase(Agameplay_loopGameModeBase&&); \
+	NO_API Agameplay_loopGameModeBase(const Agameplay_loopGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, Agameplay_loopGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(Agameplay_loopGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(Agameplay_loopGameModeBase)
+
+
+#define gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_12_PROLOG
+#define gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_SPARSE_DATA \
+	gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_RPC_WRAPPERS \
+	gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_INCLASS \
+	gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_SPARSE_DATA \
+	gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> GAMEPLAY_LOOP_API UClass* StaticClass<class Agameplay_loopGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID gameplay_loop_Source_gameplay_loop_gameplay_loopGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
